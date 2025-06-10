7182ffe86924e76c32483bb148597027d388a930
lunarollion <lunar.gultom@ollion.com>
2025-06-10 12:34:00 +0700

    update-4

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
-  features {}
-  use_oidc        = true
+  features = {}
+  use_oidc = var.use_oidc
+  client_id = var.client_id
+  tenant_id = var.tenant_id
   subscription_id = var.subscription_id
 }
