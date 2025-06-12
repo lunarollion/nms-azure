d3d5a13552801e50e078b21203dbe2bf588b81cf
lunarollion <lunar.gultom@ollion.com>
2025-06-12 10:41:55 +0700

    update

--- provider.tf
+++ provider.tf
@@ -1,9 +1,11 @@
-# Default provider (required)
 provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  use_oidc        = var.use_oidc
   features {}
 }
 
-# for customer HYC
 provider "azurerm" {
   alias           = "hyc"
   subscription_id = var.subscription_id
