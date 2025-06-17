1cd28ff9266261437eb3b6261f7c244d6d03a4dd
lunarollion <lunar.gultom@ollion.com>
2025-06-17 18:06:04 +0700

    update tfstate

--- provider.tf
+++ provider.tf
@@ -1,8 +1,9 @@
 provider "azurerm" {
+  use_oidc       = true
+  tenant_id      = var.tenant_id
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  use_oidc        = var.use_oidc
+  client_id      = var.client_id
+
   features {}
 }
 
